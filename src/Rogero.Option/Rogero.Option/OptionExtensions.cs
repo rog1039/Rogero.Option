abcd28adc283e7618e6a3c9176a98e9356e85566
Paul Rogero <progero@gmail.com>
2015-07-24 15:32:22 -0400

    Initial commit.

---
+++ src/Rogero.Option/Rogero.Option/OptionExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rogero.Option
+{
+    public static class OptionExtensions
+    {
+        public static Option<T> DoIfValue<T>(this Option<T> source, Action<T> action)
+        {
+            if (source.HasValue)
+            {
+                action(source.Value);
+            }
+            return source;
+        }
+
+        public static TRes Match<T,TRes>(this Option<T> option, Func<T, TRes> some, Func<TRes> none)
+        {
+            if (option.HasValue)
+                return some.Invoke(option.Value);
+            else
+                return none.Invoke();
+        }
+
+        public static void Match<T>(this Option<T> option, Action<T> some, Action none)
+        {
+            if (option.HasValue)
+                some.Invoke(option.Value);
+            else
+                none.Invoke();
+        }
+    }
+}
